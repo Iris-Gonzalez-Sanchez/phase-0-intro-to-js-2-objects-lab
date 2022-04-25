41e015d4693900c75069fd9c31b3dd9d64a5a81f
Iris-Gonzalez-Sanchez <Iris.GonzalezSan@Gmail.com>
2022-04-25 17:42:12 -0400

    commit tree 82e8db6214d13d22b373f43407b9f6a47a076b72

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let obj = { ...employee};
+    obj[key] = value;
+    return obj;
+}
+
+let employee = {
+    name : "Joanna",
+    streetAddress : "41- Biberry St",
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee};
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
